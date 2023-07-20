4f462a6fe5cb88b5bd016dd22c26d7b7b433acaf
wunpriyalal <137602257+wunpriyalal@users.noreply.github.com>
2023-07-21 01:34:14 +0530

    Add files via upload

---
+++ Lab10- 2023.07.14/Q1.php
@@ -0,0 +1,16 @@
+<?php
+
+$x=6;
+$y=4;
+
+$sum=$x+$y;
+$difference=$x-$y;
+$product=$x*$y;
+$division=$x/$y;
+
+echo "Sum: " .($x+$y)."<br>";
+echo "Difference: " .($x-$y)."<br>";
+echo "Product: " .($x*$y)."<br>";
+echo "Division: " .($x/$y)."<br>";
+
+?>
